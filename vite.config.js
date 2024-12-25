852b00d08d5cbdae25f18b4d30c54cb2f9bbee62
yair-shtern <101128645+yair-shtern@users.noreply.github.com>
2024-12-25 16:43:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000, // Use the Render-assigned port or default to 3000
     open: true,
     proxy: {
       // Proxy for Wikipedia API requests
